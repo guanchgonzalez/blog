40472fa8dec5a6ba3caae1d15474c7315ef8abe8
rg <rg@ruben.redbordersc.lan>
2021-02-25 16:51:27 +0000

    Código Facilito: Ruby on Rails 4 desde 0: 10 - Validaciones

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ActiveRecord::Base
+  validates :title, presence: true, uniqueness: true
+  validates :body, presence: true, length: { minimum: 20 }
+  validates :username, format: { with: /regex/ }
 end
