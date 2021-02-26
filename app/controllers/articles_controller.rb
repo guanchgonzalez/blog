d1fc0d1144dd35f90e80c0cc748ce14b9c63597f
rg <rg@ruben.redbordersc.lan>
2021-02-26 11:52:18 +0000

    Código Facilito: Ruby on Rails 4 desde 0: 12 - Strong params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,8 +17,8 @@ class ArticlesController < ApplicationController
 
   #POST "/articles"
   def create
-    @article = Article.new(title: params[:article][:title],
-                            body: params[:article][:body])
+    @article = Article.new(article_params)
+    
     if @article.save
       redirect_to @article
     else
@@ -36,5 +36,11 @@ class ArticlesController < ApplicationController
   #PUT "/articles/:id"
   def update
     # @article.update_attributes( { title: 'Nuevo titulo' } )
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title,:body)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true, uniqueness: true
   validates :body, presence: true, length: { minimum: 20 }
-  validates :username, format: { with: /regex/ }
+  #validates :username, format: { with: /regex/ }
 end
