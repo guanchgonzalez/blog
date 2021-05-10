3851b8d8bb419f36ceab400d049022a2585c1055
rg <rg@localhost.localdomain>
2021-05-10 17:13:47 +0100

    Código Facilito: Ruby on Rails 4 desde 0: 23 - Upload files (II/II)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210301165307) do
+ActiveRecord::Schema.define(version: 20210510152452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,9 +20,13 @@ ActiveRecord::Schema.define(version: 20210301165307) do
     t.string   "title"
     t.text     "body"
     t.integer  "visits_count"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "cover_file_name"
+    t.string   "cover_content_type"
+    t.integer  "cover_file_size"
+    t.datetime "cover_updated_at"
   end
 
   add_index "articles", ["user_id"], name: "index_articles_on_user_id", using: :btree
