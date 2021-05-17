913599b319c0dfa064757296c0684fbd6a7d066c
rg <rg@localhost.localdomain>
2021-05-17 17:07:37 +0100

    Código Facilito: Ruby on Rails 4 desde 0: 34 - Avatars from Gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,11 @@ class User < ActiveRecord::Base
   has_many :comments
 
   include PermissionsConcern
+
+  def avatar
+    email_address = self.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    image_src = "https://www.gravatar.com/avatar/#{hash}"
+  end
+
 end
