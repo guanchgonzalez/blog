2c65786b3674c53eae567a00531f8c49909d7941
rg <rg@localhost.localdomain>
2021-05-18 12:36:09 +0100

    Código Facilito: Ruby on Rails 4 desde 0: 35 - Application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def article_cover url, options = {}
+    html_class = options[:class]
+    html_style = "background: url(#{url});"\
+                  "width: 100%; height: 300px; background-size: cover;"
+    html = "<header class='#{html_class}' style='#{html_style}' >"\
+            "</header>"
+    html.html_safe
+  end
 end
